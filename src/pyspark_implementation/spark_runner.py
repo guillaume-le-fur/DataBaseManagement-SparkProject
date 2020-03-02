6d66cb336341b1fcc36f608af4f468bd70ca394a
guillaume <guillaume.lefur29000@gmail.com>
2020-03-02 11:57:04 +0100

    [FIX] Fixed bug of algorithm, tested and works on 1000 edges

--- src/pyspark_implementation/spark_runner.py
+++ src/pyspark_implementation/spark_runner.py
@@ -8,15 +8,16 @@ import matplotlib.pyplot as plt
 
 class SparkRunner:
 
-    def __init__(self, data_file, sep='\\s+', row_limit=None, skip_rows=None):
-        if data_file is not None:
+    def __init__(self, data_path, sep='\\s+', row_limit=None, skip_rows=None):
+        self.data_path = data_path
+        if data_path is not None:
             self.spark_context = pyspark.SparkContext()
             self.sql_context = pyspark.SQLContext(self.spark_context)
             self.initial_data = self.sql_context.createDataFrame(
-                pd.read_csv(data_file, sep=sep, nrows=row_limit, skiprows=skip_rows), ['f', 't']
+                pd.read_csv(data_path, sep=sep, nrows=row_limit, skiprows=skip_rows), ['f', 't']
             )
             self.data = self.sql_context.createDataFrame(
-                pd.read_csv(data_file, sep=sep, nrows=row_limit, skiprows=skip_rows), ['f', 't']
+                pd.read_csv(data_path, sep=sep, nrows=row_limit, skiprows=skip_rows), ['f', 't']
             )
             self.counter = 1
             self.old_data = self.sql_context.createDataFrame(pd.DataFrame([[0, 1]], columns=['f', 't']))
@@ -59,11 +60,8 @@ class SparkRunner:
         """
         if verbose:
             print('--- INPUT DATA ---')
-            self.data.show()
+            self.data.show(self.data.count(), False)
         data = self.map(data)
-        # TODO See if counter has to be used this way.
-        self.counter = np.abs(self.old_data.count() - data.count())
-        self.old_data = data
         grouped_data = data.groupBy('f') \
             .agg(f.min('t').alias('min_t'))
         first_map_reduce_output = data.join(
@@ -71,25 +69,50 @@ class SparkRunner:
             'f',
             'inner'
         )
-        reduce_out = first_map_reduce_output \
-            .withColumn(
-                'max_f_t',
-                f.greatest(
-                    first_map_reduce_output.f,
-                    first_map_reduce_output.t
-                )
-            ) \
-            .selectExpr('max_f_t as f', 'min_t as t') \
-            .filter('f > t') \
-            .dropDuplicates() \
-            .groupBy('f')\
-            .agg(f.min('t').alias('t'))\
-            .sort(['f', 't'])
+        if verbose:
+            print('--- MAP OUTPUT ---')
+            first_map_reduce_output.show(first_map_reduce_output.count(), False)
+        # TODO See if counter has to be used this way.
+        self.counter = np.abs(self.old_data.count() - first_map_reduce_output.count())
+        self.old_data = first_map_reduce_output
+
+        reduce_out_tmp = first_map_reduce_output\
+            .filter('f > min_t')
+
+        reduce_out = grouped_data\
+            .selectExpr('f as f', 'min_t as t').union(
+                reduce_out_tmp.selectExpr('t as f', 'min_t as t')
+            ).sort(['f', 't'])
+
+        if verbose:
+            print('--- INTERMEDIATE ---')
+            reduce_out.show(reduce_out.count(), False)
+
+        reduce_out = reduce_out\
+            .dropDuplicates(['f', 't'])\
+            .filter('f > t')
+
+        # reduce_out = first_map_reduce_output \
+        #     .withColumn(
+        #         'max_f_t',
+        #         f.greatest(
+        #             first_map_reduce_output.f,
+        #             first_map_reduce_output.t
+        #         )
+        #     ) \
+        #     .selectExpr('max_f_t as f', 'min_t as t') \
+        #     .dropDuplicates() \
+        #     .sort(['f', 't'])
+        #     .filter('f > t') \
+        #
+        #     .groupBy('f')\
+        #     .agg(f.min('t').alias('t'))\
+        #     .sort(['f', 't'])
         self.data = reduce_out
 
         if verbose:
             print('--- REDUCE OUTPUT ---')
-            reduce_out.show()
+            reduce_out.show(reduce_out.count(), False)
             print(f'Value of the counter : {self.counter}')
         return reduce_out
 
@@ -107,9 +130,14 @@ class SparkRunner:
         self.final_output = reduce_out
         return reduce_out
 
-    def save_output(self):
+    def save_input(self, file_name):
+        self.initial_data.toPandas().to_csv(f'out/spark_out/{file_name}', index=False)
+
+    def save_output(self, file_name=None):
+        if file_name is None:
+            file_name = 'output.csv'
         if self.final_output:
-            self.final_output.toPandas().to_csv('./out/spark_out/output.csv', index=False)
+            self.final_output.toPandas().to_csv(f'./out/spark_out/{file_name}', index=False)
 
     def load_output(self):
         self.final_output = self.sql_context.createDataFrame(pd.read_csv('./out/spark_out/output.csv'))
@@ -121,6 +149,7 @@ class SparkRunner:
         graph = nx.Graph()
         from_nodes = pd_initial_data.loc[:, 'f'].unique()
         to_nodes = pd_initial_data.loc[:, 't'].unique()
+        all_nodes = list(set(list(from_nodes) + list(to_nodes)))
         distinct_clusters = list(pd_output_data.loc[:, 't'].unique())
         number_clusters = len(distinct_clusters)
         colors = {}
@@ -128,24 +157,19 @@ class SparkRunner:
         for cluster in distinct_clusters:
             colors[cluster] = color_palette[palette_index]
             palette_index = (palette_index + 1) % len(color_palette)
-        print(f'Color palette : {colors}')
-        all_nodes = list(set(list(from_nodes) + list(to_nodes)))
         color_set = []
         for node in all_nodes:
             graph.add_node(node)
             if node in distinct_clusters:
                 color_set.append(colors[node])
             else:
-                print(f'Node {node}')
                 row = pd_output_data[pd_output_data['f'] == node]
-                print(row)
                 # TODO debug nodes that don't have a cluster
-                if row.shape[0]> 0:
+                if row.shape[0] > 0:
                     cluster = row.values[0][1]
-                    print(f'Node : {node}, cluster {cluster}')
                     color_set.append(colors[cluster])
                 else:
-                    color_set.append('black')
+                    color_set.append('grey')
 
         for index, edge in pd_initial_data.iterrows():
             graph.add_edge(edge[0], edge[1])
@@ -167,6 +191,9 @@ class SparkRunner:
 # # r.save_output()
 # r.plot_graph()
 
-r2 = SparkRunner('web-Google.txt', row_limit=100, skip_rows=6) # TODO check exact value, not 6
+r2 = SparkRunner('web-Google.txt', row_limit=1000, skip_rows=4) # TODO check exact value, not 6
 r2.run()
+# verbose=True
+r2.save_input('in_google.csv')
+r2.save_output('out_google.csv')
 r2.plot_graph()
